f34d280ec56d6137ee0074decaf5f9c55285d204
Adam Berkeš <adam.berkes@koala42.com>
2024-11-19 16:55:55 +0100

    Add gradual timeout to camera boot

--- robopipe-api/camera/camera.py
+++ robopipe-api/camera/camera.py
@@ -1,8 +1,11 @@
 import depthai as dai
 
+import time
+
 from ..error import CameraShutDownException
+from ..log import logger
 from .camera_stats import CameraStats
-from .pipeline import Pipeline, NNPipeline, PipelineQueueType
+from .pipeline import Pipeline, NNPipeline
 from .nn import CameraNNConfig
 from .sensor import Sensor
 
@@ -26,6 +29,18 @@ class Camera:
     def __del__(self):
         self.close()
 
+    def __boot_camera(self, retries: int = 5, timeout_base: float = 1):
+        timeout = timeout_base
+        for _ in range(retries):
+            try:
+                self.camera_handle = dai.Device(
+                    self.pipeline.pipeline, dai.DeviceInfo(self.mxid)
+                )
+                return
+            except:
+                time.sleep(timeout)
+                timeout *= 2
+
     def __get_sensor_queues(self, sensor_name: str, q_type_input: bool):
         get_queue = (
             self.camera_handle.getInputQueue
@@ -36,9 +51,12 @@ class Camera:
         return {
             k: get_queue(v)
             for k, v in (
-                self.pipeline.input_queues.get(sensor_name)
-                if q_type_input
-                else self.pipeline.output_queues.get(sensor_name)
+                (
+                    self.pipeline.input_queues.get(sensor_name)
+                    if q_type_input
+                    else self.pipeline.output_queues.get(sensor_name)
+                )
+                or {}
             ).items()
         }
 
@@ -52,10 +70,10 @@ class Camera:
             if sensor_name in self.pipeline.cameras:
                 output_queues = self.__get_sensor_queues(sensor_name, False)
 
-                for queue_type, queue in output_queues.items():
-                    if queue_type in [PipelineQueueType.STILL]:
-                        queue.setBlocking(False)
-                        queue.setMaxSize(1)
+                # for queue_type, queue in output_queues.items():
+                #     if queue_type in [PipelineQueueType.STILL]:
+                #         queue.setBlocking(False)
+                #         queue.setMaxSize(1)
 
                 self.sensors[sensor_name] = Sensor(
                     self.pipeline.cameras[sensor_name],
@@ -69,18 +87,19 @@ class Camera:
             self.camera_handle.close()
             self.camera_handle = None
             self.pipeline = None
+            logger.debug(f"Closed camera {self.mxid}")
 
     def open(self, pipeline: Pipeline):
-        device_info = dai.DeviceInfo(self.mxid)
         self.close()
-        self.camera_handle = dai.Device(pipeline.pipeline, device_info)
         self.pipeline = pipeline
+        self.__boot_camera()
         self.reload_sensors()
+        logger.debug(f"Opened camera {self.mxid}")
 
         return self
 
     def deploy_nn(self, nn: CameraNNConfig):
-        self.pipeline = NNPipeline([nn])
+        self.pipeline = NNPipeline([nn], self.pipeline.pipeline)
 
         self.open(self.pipeline)
 
