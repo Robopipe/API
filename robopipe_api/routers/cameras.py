5068bb6fbf3612230af7c65fb26737b3a0ad8886
Adam Berkeš <adam.berkes@koala42.com>
2024-11-25 16:51:30 +0100

    Separate cameras router and single camera router

--- robopipe_api/routers/cameras.py
+++ robopipe_api/routers/cameras.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from ..camera.camera_stats import CameraStats
 from ..camera.device_info import DeviceInfo
 from .common import CameraManagerDep, Mxid, CameraDep
 
@@ -11,25 +12,35 @@ def list_cameras(camera_manager: CameraManagerDep) -> list[DeviceInfo]:
     return list(map(lambda x: x.info, camera_manager.cameras.values()))
 
 
-@router.get("/{mxid}")
+camera_router = APIRouter(
+    prefix="/{mxid}",
+    tags=["cameras"],
+    responses={404: {"description": "Camera not found"}},
+)
+
+
+@camera_router.get("/")
 def get_camera(camera: CameraDep) -> DeviceInfo:
     return camera.info
 
 
-@router.post("/{mxid}")
+@camera_router.post("/")
 def create_camera(camera_manager: CameraManagerDep, mxid: Mxid) -> DeviceInfo:
     camera_manager.boot_camera(mxid)
 
     return {"status": "ok"}
 
 
-@router.delete("/{mxid}")
+@camera_router.delete("/")
 def delete_camera(camera_manager: CameraManagerDep, mxid: Mxid) -> DeviceInfo:
     camera_manager.shutdown_camera(mxid)
 
     return {"status": "ok"}
 
 
-@router.get("/{mxid}/stats")
-def get_camera_stats(camera: CameraDep):
+@camera_router.get("/stats")
+def get_camera_stats(camera: CameraDep) -> CameraStats:
     return camera.stats
+
+
+router.include_router(camera_router)
