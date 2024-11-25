ff1628cd308adfdfccd3506b3d761a78145c84ea
Adam Berkeš <adam.berkes@koala42.com>
2024-11-25 16:48:23 +0100

    Add support for BGR888i format conversion

--- robopipe_api/utils/image.py
+++ robopipe_api/utils/image.py
@@ -14,5 +14,7 @@ def img_frame_to_pil_image(img_frame: dai.ImgFrame) -> Image.Image:
         return Image.fromarray(img_frame.getFrame(), "L")
     elif img_type == dai.RawImgFrame.Type.NV12:
         return av.VideoFrame.from_ndarray(img_frame.getFrame(), "nv12").to_image()
+    elif img_type == dai.RawImgFrame.Type.BGR888i:
+        return Image.fromarray(img_frame.getFrame()[..., ::-1])
     else:
-        raise UnsupportedImageFormat()
+        raise UnsupportedImageFormat(f"Given format: {img_type}")
