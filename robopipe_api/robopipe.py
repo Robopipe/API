80079008cad87c9659503aedf2bb077dbcb41d2a
Adam Berkeš <adam.berkes@koala42.com>
2024-11-27 09:53:31 +0100

    Add dotenv config

--- robopipe_api/robopipe.py
+++ robopipe_api/robopipe.py
@@ -1,25 +1,27 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import uvicorn
 
 from contextlib import asynccontextmanager
+import os
 
-from .camera.camera_manager import CameraManager
+from .camera.camera_manager import camera_manager_factory
 from .error import (
     CameraNotFoundException,
     SensorNotFoundException,
 )
 from .routers import cameras, sensors
-from .stream import StreamService
+from .stream import stream_service_factory
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
 
-    camera_manager = CameraManager()
+    camera_manager = camera_manager_factory()
     camera_manager.boot_cameras()
-    stream_service = StreamService()
+    stream_service = stream_service_factory(camera_manager)
 
     yield
 
@@ -35,14 +37,6 @@ app = FastAPI(
 app.include_router(cameras.router)
 app.include_router(sensors.router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 @app.exception_handler(Exception)
 def global_exception_handler(request: Request, exc: Exception):
@@ -50,5 +44,24 @@ def global_exception_handler(request: Request, exc: Exception):
         return JSONResponse(status_code=404, content=None)
 
 
+def setup():
+    load_dotenv()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=(os.environ.get("CORS_ORIGINS") or "*").split(","),
+        allow_credentials=True,
+        allow_methods=(os.environ.get("CORS_METHODS") or "*").split(","),
+        allow_headers=(os.environ.get("CORS_HEADERS") or "*").split(","),
+    )
+
+
+setup()
+
+
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        app,
+        host=os.environ.get("HOST") or "0.0.0.0",
+        port=int(os.environ.get("PORT") or "8080"),
+    )
