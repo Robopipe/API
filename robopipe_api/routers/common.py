5e158c5f62e064648d9962a5405fa24196d1a07f
Adam Berkeš <adam.berkes@koala42.com>
2024-12-11 16:13:32 +0100

    Add controller routers

--- robopipe_api/routers/common.py
+++ robopipe_api/routers/common.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, Path
+from fastapi import Depends, Path, Request
 
 from typing import Annotated
 
@@ -6,6 +6,8 @@ from ..camera.camera import Camera
 from ..camera.camera_manager import CameraManager, camera_manager_factory
 from ..camera.sensor import Sensor
 from ..stream import StreamService, stream_service_factory
+from ..controller.devices import DeviceList, Devices, Device
+from ..controller.devices import *
 
 CameraManagerDep = Annotated[CameraManager, Depends(camera_manager_factory)]
 Mxid = Annotated[str, Path(regex=r"[A-Z0-9]+")]
@@ -31,3 +33,32 @@ def get_stream_service(camera_manager: CameraManagerDep):
 
 
 StreamServiceDep = Annotated[StreamService, Depends(get_stream_service)]
+
+DEVICE_TYPES = [
+    DI,
+    RO,
+    DO,
+    AI,
+    AO,
+    SENSOR,
+    LED,
+    WATCHDOG,
+    MODBUS_SLAVE,
+    OWPOWER,
+    REGISTER,
+    DATA_POINT,
+    OWBUS,
+    DEVICE_INFO,
+]
+
+DevicesDep = Annotated[DeviceList, Depends(lambda: Devices)]
+DeviceType = Annotated[str, Path(regex=r"{}".format("|".join(DEVICE_TYPES)))]
+
+
+def get_device(devices: DevicesDep, circuit: str, request: Request):
+    device_type = request.url.path.split("/")[2]
+
+    return devices.by_name(device_type, circuit)
+
+
+DeviceDep = Annotated[Device, Depends(get_device)]
