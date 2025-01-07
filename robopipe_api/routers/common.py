02708c72b07f01b2c9f3ab2a73852dad97f03bf4
Adam Berkeš <adam.berkes@koala42.com>
2025-01-07 16:00:50 +0100

    Add api support for YOLO and MobileNet NNs

--- robopipe_api/routers/common.py
+++ robopipe_api/routers/common.py
@@ -1,10 +1,14 @@
-from fastapi import Depends, Path, Request
+from fastapi import Depends, Path, Request, Form, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+
+from pydantic import ValidationError
 
 from typing import Annotated
 
 from ..camera.camera import Camera
 from ..camera.camera_manager import CameraManager, camera_manager_factory
 from ..camera.sensor import Sensor
+from ..models.nn_config import NNConfig, NNType, NNYoloConfig, NNMobileNetConfig
 from ..stream import StreamService, stream_service_factory
 from ..controller.devices import DeviceList, Devices, Device
 from ..controller.devices import *
@@ -62,3 +66,32 @@ def get_device(devices: DevicesDep, circuit: str, request: Request):
 
 
 DeviceDep = Annotated[Device, Depends(get_device)]
+
+
+def nn_config_checker(nn_config: Annotated[str, Form()]):
+    try:
+        model = NNConfig.model_validate_json(nn_config)
+
+        if model.type == NNType.Generic and model.nn_config is not None:
+            raise ValueError("nn_config must be null when NNType is Generic")
+        elif model.type == NNType.YOLO and not isinstance(
+            model.nn_config, NNYoloConfig
+        ):
+            raise ValueError(
+                "nn_config must be of type NNYoloConfig or null when NNType is YOLO"
+            )
+        elif model.type == NNType.MobileNet and not isinstance(
+            model.nn_config, NNMobileNetConfig
+        ):
+            raise ValueError(
+                "nn_config must be of type NNMobileNetConfig or null when NNType is MobileNet"
+            )
+
+        return model
+    except ValidationError as e:
+        raise HTTPException(
+            status.HTTP_422_UNPROCESSABLE_ENTITY, jsonable_encoder(e.errors())
+        )
+
+
+NNConfigDep = Annotated[NNConfig, Depends(nn_config_checker)]
