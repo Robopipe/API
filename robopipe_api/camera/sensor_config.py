48251a61ddf24723fdb87f16b1062ab87b6ba498
Adam Berkeš <adam.berkes@koala42.com>
2025-01-08 09:07:11 +0100

    Rework camera resolutions to human readable format

--- robopipe_api/camera/sensor_config.py
+++ robopipe_api/camera/sensor_config.py
@@ -1,6 +1,7 @@
 import depthai as dai
 
 from dataclasses import dataclass, asdict
+from enum import Enum
 
 CameraNode = dai.node.Camera | dai.node.ColorCamera | dai.node.MonoCamera
 
@@ -8,11 +9,9 @@ CameraNode = dai.node.Camera | dai.node.ColorCamera | dai.node.MonoCamera
 @dataclass
 class ConfigProperties:
     @classmethod
-    def parse_from_node(cls, node: CameraNode):
-        pass
+    def parse_from_node(cls, node: CameraNode): ...
 
-    def update_node(self, node: CameraNode):
-        pass
+    def update_node(self, node: CameraNode): ...
 
 
 @dataclass
@@ -41,43 +40,77 @@ class CameraConfigProperties(ConfigProperties):
         node.setFps(self.fps)
 
 
+class ColorCameraResolution(Enum):
+    THE_1080_P = dai.ColorCameraProperties.SensorResolution.THE_1080_P.name
+    THE_1200_P = dai.ColorCameraProperties.SensorResolution.THE_1200_P.name
+    THE_12_MP = dai.ColorCameraProperties.SensorResolution.THE_12_MP.name
+    THE_1352X1012 = dai.ColorCameraProperties.SensorResolution.THE_1352X1012.name
+    THE_13_MP = dai.ColorCameraProperties.SensorResolution.THE_13_MP.name
+    THE_1440X1080 = dai.ColorCameraProperties.SensorResolution.THE_1440X1080.name
+    THE_2024X1520 = dai.ColorCameraProperties.SensorResolution.THE_2024X1520.name
+    THE_4000X3000 = dai.ColorCameraProperties.SensorResolution.THE_4000X3000.name
+    THE_48_MP = dai.ColorCameraProperties.SensorResolution.THE_48_MP.name
+    THE_4_K = dai.ColorCameraProperties.SensorResolution.THE_4_K.name
+    THE_5312X6000 = dai.ColorCameraProperties.SensorResolution.THE_5312X6000.name
+    THE_5_MP = dai.ColorCameraProperties.SensorResolution.THE_5_MP.name
+    THE_720_P = dai.ColorCameraProperties.SensorResolution.THE_720_P.name
+    THE_800_P = dai.ColorCameraProperties.SensorResolution.THE_800_P.name
+
+
 @dataclass
 class ColorCameraConfigProperties(ConfigProperties):
-    resolution: int  # dai.ColorCameraProperties.SensorResolution.value
+    resolution: ColorCameraResolution
     still_size: tuple[int, int]
     preview_size: tuple[int, int]
     video_size: tuple[int, int]
+    sensor_crop: tuple[float, float]
     fps: float
 
     @classmethod
     def parse_from_node(cls, node: dai.node.ColorCamera):
         return cls(
-            node.getResolution().value,
+            ColorCameraResolution(node.getResolution().name),
             node.getStillSize(),
             node.getPreviewSize(),
             node.getVideoSize(),
+            node.getSensorCrop(),
             node.getFps(),
         )
 
     def update_node(self, node: dai.node.ColorCamera):
-        node.setResolution(dai.ColorCameraProperties.SensorResolution(self.resolution))
+        node.setResolution(
+            dai.ColorCameraProperties.SensorResolution.__members__[
+                self.resolution.value
+            ]
+        )
         node.setStillSize(self.still_size)
         node.setPreviewSize(self.preview_size)
         node.setVideoSize(self.video_size)
+        node.setSensorCrop(*self.sensor_crop)
         node.setFps(self.fps)
+
+
+class MonoCameraResolution(Enum):
+    THE_1200_P = dai.MonoCameraProperties.SensorResolution.THE_1200_P.name
+    THE_400_P = dai.MonoCameraProperties.SensorResolution.THE_400_P.name
+    THE_480_P = dai.MonoCameraProperties.SensorResolution.THE_480_P.name
+    THE_720_P = dai.MonoCameraProperties.SensorResolution.THE_720_P.name
+    THE_800_P = dai.MonoCameraProperties.SensorResolution.THE_800_P.name
 
 
 @dataclass
 class MonoCameraConfigProperties(ConfigProperties):
-    resolution: int  # dai.MonoCameraProperties.SensorResolution.value
+    resolution: MonoCameraResolution
     fps: float
 
     @classmethod
     def parse_from_node(cls, node: dai.node.MonoCamera):
-        return cls(node.getResolution().value, node.getFps())
+        return cls(MonoCameraResolution(node.getResolution().name), node.getFps())
 
     def update_node(self, node: dai.node.MonoCamera):
-        node.setResolution(dai.MonoCameraProperties.SensorResolution(self.resolution))
+        node.setResolution(
+            dai.MonoCameraProperties.SensorResolution.__members__[self.resolution.value]
+        )
         node.setFps(self.fps)
 
 
@@ -103,7 +136,7 @@ class SensorConfig:
     @properties.setter
     def properties(self, value: dict):
         CONFIG_PROPERTIES[type(self.camera_node)](
-            **(asdict(self.properties) | value)
+            **(asdict(self.properties) | asdict(value))
         ).update_node(self.camera_node)
 
 
