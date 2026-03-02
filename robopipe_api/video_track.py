a4fb56d6b06f034c3ad2c79bbc9eead0fcfddce3
Adam Berkeš <adam.berkes@koala42.com>
2026-03-02 14:38:32 +0100

    Edit dashboard item limits

--- robopipe_api/video_track.py
+++ robopipe_api/video_track.py
@@ -1,4 +1,4 @@
-from aiortc import VideoStreamTrack
+from aiortc import VideoStreamTrack, MediaStreamError
 from aiortc.contrib.media import MediaRelay
 import anyio.to_thread
 from av import VideoFrame
@@ -6,6 +6,7 @@ from av import VideoFrame
 from functools import lru_cache
 
 from .camera.sensor.sensor_base import SensorBase
+from .log import logger
 
 
 class VideoTrack(VideoStreamTrack):
@@ -15,9 +16,17 @@ class VideoTrack(VideoStreamTrack):
         self._start: float | None = None
 
     async def recv(self) -> VideoFrame:
-        frame = await anyio.to_thread.run_sync(
-            self.sensor.get_video_frame, abandon_on_cancel=True
-        )
+        try:
+            frame = await anyio.to_thread.run_sync(
+                self.sensor.get_video_frame, abandon_on_cancel=True
+            )
+        except Exception as e:
+            logger.error(f"Error in get_video_frame: {e}")
+            self.stop()
+            video_track_factory.cache_clear()
+            media_relay_factory.cache_clear()
+            raise MediaStreamError()
+
         frame.pts, frame.time_base = await self.next_timestamp()
 
         return frame
