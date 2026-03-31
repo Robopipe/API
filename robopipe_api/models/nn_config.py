af782c40b9366b7cbaeafc7073091e142e4953fc
Adam Berkeš <adam.berkes@koala42.com>
2026-03-31 10:25:05 +0200

    Implement custom dashboard counters

--- robopipe_api/models/nn_config.py
+++ robopipe_api/models/nn_config.py
@@ -1,7 +1,4 @@
-from pydantic import model_validator
-
 from enum import Enum
-from typing import Any
 
 from .base_model import BaseModel
 
@@ -14,19 +11,10 @@ class NNType(Enum):
 
 class NNGenericConfig(BaseModel):
     use_parser: bool = True
-    pass
-
-
-class NNDetectionConfig(BaseModel):
-    pass
-
-
-class NNSpatialDetectionConfig(BaseModel):
-    pass
 
 
 class NNConfig(BaseModel):
     type: NNType
     model_id: int | None = None
     num_inference_threads: int = 2
-    nn_config: NNGenericConfig | NNDetectionConfig | NNSpatialDetectionConfig = None
+    nn_config: NNGenericConfig | None = None
